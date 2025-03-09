ea0280e1cad34f7bd2260a0b620338c68db3deee
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-03-08 23:48:20 -0500

    up

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -16,7 +16,9 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
       <h2>Are you sure?</h2>
       <p>Do you really want to remove this place?</p>
       <div id="confirmation-actions">
-        <button onClick={onCancel} className="button-text"></button>
+        <button onClick={onCancel} className="button-text">
+          No
+        </button>
         <button onClick={onConfirm} className="button">
           Yes
         </button>
